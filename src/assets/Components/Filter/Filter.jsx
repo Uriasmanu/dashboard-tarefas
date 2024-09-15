bacf7c18ca9d26ebfdfc555400da6dd0508848b4
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-09-14 22:45:45 -0300

    .

--- src/assets/Components/Filter/Filter.jsx
+++ src/assets/Components/Filter/Filter.jsx
@@ -4,17 +4,10 @@ import filtro from '../../../imagem/filter.svg'
 
 const Filter = () => {
     return (
-<<<<<<< HEAD
         <button className="ContainerFilter">
             <img src={filtro} alt="" />
             <p>Filtro</p>
         </button>
-=======
-        <div className="ContainerFilter">
-            <img src={filtro} alt="" />
-            <p>Filtro</p>
-        </div>
->>>>>>> c8fd2ad199c7c50dd49a0693b4ef746790500444
     )
 }
 
